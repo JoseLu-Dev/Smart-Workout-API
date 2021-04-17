f7c9201617f8e156dc3e6713e9016590aff0b479
JoseLu-Dev <joseludev@gmail.com>
2021-04-18 01:04:19 +0200

    User controller added

---
+++ src/app/user/user.controller.js
@@ -0,0 +1,11 @@
+const BaseController = require('../common/base.controller')
+const UserModel = require('./user.model')
+class UserController extends BaseController {
+
+    constructor(){
+      super(UserModel)
+    }
+
+}
+
+module.exports = UserController
