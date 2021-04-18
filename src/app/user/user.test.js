fb5ac2cd4d3132723142005aedfe682afb0f9402
JoseLu-Dev <joseludev@gmail.com>
2021-04-18 04:46:19 +0200

    Added simple user tests

--- src/app/user/user.test.js
+++ src/app/user/user.test.js
@@ -1,1 +1,26 @@
-//TODO: add tests
+const mongoose = require('mongoose')
+const { server } = require('../../server')
+
+const User = require('../user/user.model')
+
+const {
+    api
+} = require('../common/helpers.testing')
+
+beforeEach(async () => {
+    await User.deleteMany({})
+})
+
+describe('Simple user queries', () => {
+    test('Get users', async () => {
+        await api
+            .get('/users')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+    server.close()
+})
