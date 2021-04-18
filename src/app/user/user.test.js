743551393a9bf8c495b7aa03861a93081e2f0148
JoseLu-Dev <joseludev@gmail.com>
2021-04-19 00:08:36 +0200

    Modified user test to test jwt

--- src/app/user/user.test.js
+++ src/app/user/user.test.js
@@ -11,13 +11,31 @@ beforeEach(async () => {
     await User.deleteMany({})
 })
 
-describe('Simple user queries', () => {
-    test('Get users', async () => {
+describe('Get user data', () => {
+    test('Ensure you can get user data sending a valid token', async () => {
+        const user = {
+            name: "JoseLuDev",
+            email: "joseludev@gmail.com",
+            password: "securePassword",
+            status: "Active"
+        }
+
+        newUser = await new User(user).save()
+
+        const response = await api
+            .post('/auth/login')
+            .send(user)
+            .expect(200)
+
+        authToken = response.body.token
+
         await api
             .get('/users')
+            .set('Authorization', 'bearer ' + authToken)
             .expect(200)
             .expect('Content-Type', /application\/json/)
     })
+
 })
 
 afterAll(() => {
