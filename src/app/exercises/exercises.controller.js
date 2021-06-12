9ddbc8c67ec012843827921c607329d8fa8a589e
JoseLu-Dev <joseludev@gmail.com>
2021-06-12 18:23:27 +0200

    Changed update behaviour

--- src/app/exercises/exercises.controller.js
+++ src/app/exercises/exercises.controller.js
@@ -13,18 +13,6 @@ class ExercisesController extends BaseController {
      * @param {*} res 
      */
     put = async (req, res) => {
-        const currentExercise = await this.model.findOne({ name: req.body.name })
-        if(currentExercise){ 
-            if(currentExercise.variations){
-                req.body.variations = currentExercise.variations.concat(req.body.variations)
-            }
-            if(currentExercise.progressions){
-                req.body.progressions = currentExercise.progressions.concat(req.body.progressions)
-            }
-        }
-        console.log(currentExercise)
-
-
         this.model.updateOne({ name: req.body.name }, req.body, { upsert: true }, (err) => {
             if (err) {
                 return console.error(err);
